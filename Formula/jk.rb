1827a8b4830e5d5659639a41ca3b82b2024ae9f7
Josh McKinney <joshka@users.noreply.github.com>
2026-06-23 14:53:31 -0700

    Update jk formula positioning (#12)

--- Formula/jk.rb
+++ Formula/jk.rb
@@ -1,5 +1,5 @@
 class Jk < Formula
-  desc "Log-first terminal UI for Jujutsu"
+  desc "Jj-native terminal UI for Jujutsu"
   homepage "https://github.com/joshka/jk"
   url "https://static.crates.io/crates/jk/jk-0.2.6.crate"
   sha256 "8041f691dd70273687ebe414af89af195be7905471075cda30341ee2a9871fe7"
@@ -21,6 +21,6 @@ class Jk < Formula
 
   test do
     assert_match "jk #{version}", shell_output("#{bin}/jk --version")
-    assert_match "A log-first terminal UI for Jujutsu", shell_output("#{bin}/jk --help")
+    assert_match "A jj-native terminal UI for Jujutsu", shell_output("#{bin}/jk --help")
   end
 end
